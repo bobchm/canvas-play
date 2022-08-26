269e393e98b7048ec781111dcdadc61b695a78c3
Bob Cunningham <bobchm@gmail.com>
2022-08-25 20:57:35 -0400

    fixed weird bug with not being able to select and added some selection management

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useRef, useState, useEffect } from "react";
 import Box from "@mui/material/Box";
 import Drawer from "@mui/material/Drawer";
 import AppBar from "@mui/material/AppBar";
@@ -7,6 +7,7 @@ import Toolbar from "@mui/material/Toolbar";
 import List from "@mui/material/List";
 import Typography from "@mui/material/Typography";
 import ListItem from "@mui/material/ListItem";
+import Paper from "@mui/material/Paper";
 import ListItemButton from "@mui/material/ListItemButton";
 import ListItemIcon from "@mui/material/ListItemIcon";
 import ListItemText from "@mui/material/ListItemText";
@@ -16,23 +17,61 @@ import ChangeHistoryIcon from "@mui/icons-material/ChangeHistory";
 import { fabric } from "fabric";
 
 const drawerWidth = 100;
+const appBarHeight = 64;
 
 const App = () => {
-    const [canvas, setCanvas] = useState("");
+    const canvas = useRef(null);
+    const [title, setTitle] = useState("No selection");
 
     useEffect(() => {
-        var cnv = initCanvas();
-        console.log("canvas: ", cnv);
-        setCanvas(cnv);
+        canvas.current = initCanvas();
+
+        // destroy fabric on unmount
+        return () => {
+            canvas.current.dispose();
+            canvas.current = null;
+        };
     }, []);
 
+    function describeSelection(objs) {
+        var str = "";
+        for (var i = 0; i < objs.length; i++) {
+            if (i > 0) {
+                str += " and ";
+            }
+            str += objs[i].type;
+        }
+        setTitle(str);
+    }
+
+    function handleSelectionCreated(obj) {
+        describeSelection(obj.selected);
+    }
+
+    function handleSelectionUpdated(obj) {
+        var objs = canvas.current.getActiveObjects();
+        describeSelection(objs);
+    }
+
+    function handleSelectionClear(obj) {
+        setTitle("No selection");
+    }
+
     function initCanvas() {
-        return new fabric.Canvas("canvas", {
-            height: window.innerHeight,
+        var cnv = new fabric.Canvas("canvas", {
+            top: appBarHeight,
+            height: window.innerHeight - appBarHeight,
             width: window.innerWidth - drawerWidth,
             backgroundColor: "azure",
+            selection: true,
             renderOnAddRemove: true,
         });
+        cnv.on({
+            "selection:updated": handleSelectionUpdated,
+            "selection:created": handleSelectionCreated,
+            "selection:cleared": handleSelectionClear,
+        });
+        return cnv;
     }
 
     const onSelectSquare = (cnv) => {
@@ -68,70 +107,73 @@ const App = () => {
     };
 
     return (
-        <Box sx={{ display: "flex" }}>
-            <CssBaseline />
-            {/* <AppBar
+        <div>
+            <AppBar
                 position="fixed"
                 sx={{ zIndex: (theme) => theme.zIndex.drawer + 1 }}
             >
                 <Toolbar>
                     <Typography variant="h6" noWrap component="div">
-                        Clipped drawer
+                        {title}
                     </Typography>
                 </Toolbar>
-            </AppBar> */}
-            <Drawer
-                variant="permanent"
-                sx={{
-                    width: drawerWidth,
-                    flexShrink: 0,
-                    [`& .MuiDrawer-paper`]: {
+            </AppBar>
+            <Box sx={{ display: "flex" }}>
+                <Drawer
+                    variant="permanent"
+                    sx={{
                         width: drawerWidth,
-                        boxSizing: "border-box",
-                    },
-                }}
-            >
-                <Toolbar />
-                <Box sx={{ overflow: "auto" }}>
-                    <List>
-                        <ListItem
-                            key={1}
-                            disablePadding
-                            onClick={() => onSelectSquare(canvas)}
-                        >
-                            <ListItemButton>
-                                <ListItemIcon>
-                                    <CropSquareIcon />
-                                </ListItemIcon>
-                            </ListItemButton>
-                        </ListItem>
-                        <ListItem
-                            key={2}
-                            disablePadding
-                            onClick={() => onSelectCircle(canvas)}
-                        >
-                            <ListItemButton>
-                                <ListItemIcon>
-                                    <PanoramaFishEyeIcon />
-                                </ListItemIcon>
-                            </ListItemButton>
-                        </ListItem>
-                        <ListItem
-                            key={3}
-                            disablePadding
-                            onClick={() => onSelectTriangle(canvas)}
-                        >
-                            <ListItemButton>
-                                <ListItemIcon>
-                                    <ChangeHistoryIcon />
-                                </ListItemIcon>
-                            </ListItemButton>
-                        </ListItem>
-                    </List>
-                </Box>
-            </Drawer>
-            <canvas id="canvas" />
-        </Box>
+                        flexShrink: 0,
+                        [`& .MuiDrawer-paper`]: {
+                            width: drawerWidth,
+                            boxSizing: "border-box",
+                        },
+                    }}
+                >
+                    <Toolbar />
+                    <Box sx={{ overflow: "auto" }}>
+                        <List>
+                            <ListItem
+                                key={1}
+                                disablePadding
+                                onClick={() => onSelectSquare(canvas.current)}
+                            >
+                                <ListItemButton>
+                                    <ListItemIcon>
+                                        <CropSquareIcon />
+                                    </ListItemIcon>
+                                </ListItemButton>
+                            </ListItem>
+                            <ListItem
+                                key={2}
+                                disablePadding
+                                onClick={() => onSelectCircle(canvas.current)}
+                            >
+                                <ListItemButton>
+                                    <ListItemIcon>
+                                        <PanoramaFishEyeIcon />
+                                    </ListItemIcon>
+                                </ListItemButton>
+                            </ListItem>
+                            <ListItem
+                                key={3}
+                                disablePadding
+                                onClick={() => onSelectTriangle(canvas.current)}
+                            >
+                                <ListItemButton>
+                                    <ListItemIcon>
+                                        <ChangeHistoryIcon />
+                                    </ListItemIcon>
+                                </ListItemButton>
+                            </ListItem>
+                        </List>
+                    </Box>
+                </Drawer>
+                <div style={{ marginTop: appBarHeight }}>
+                    <canvas id="canvas" />
+                </div>
+            </Box>
+        </div>
     );
 };
 
