cda9b88d96ff1b20de800901865a54e62778c3ac
Bob Cunningham <bobchm@gmail.com>
2022-10-05 15:34:18 -0400

    adding symbol to symbol button, part way through

--- src/utils/symbol-button.js
+++ src/utils/symbol-button.js
@@ -1,4 +1,5 @@
 import { fabric } from "fabric";
+import { defaultImageData, errorImageData } from "./image-defaults";
 
 var SymbolButton = fabric.util.createClass(fabric.Rect, {
     type: "symbolButton",
@@ -11,6 +12,9 @@ var SymbolButton = fabric.util.createClass(fabric.Rect, {
         this.set("label", label || "");
         this.setFont(options);
         this.textColor = options.textColor || "black";
+        this.image = new Image();
+        this.setImageSource(options.imageSource);
+        this.killScaling();
     },
 
     setFont: function (spec) {
@@ -30,6 +34,11 @@ var SymbolButton = fabric.util.createClass(fabric.Rect, {
         this.makeDirty();
     },
 
+    setImageSource: function (src) {
+        this.image.src = src || defaultImageData;
+        this.makeDirty();
+    },
+
     buildFont: function (spec) {
         var fontSpec = "";
 
@@ -45,6 +54,17 @@ var SymbolButton = fabric.util.createClass(fabric.Rect, {
         this.set("font", fontSpec);
     },
 
+    killScaling: function () {
+        this.on("scaling", () => {
+            this.width = Math.max(this.width * this.scaleX, 5);
+            this.height = Math.max(this.height * this.scaleY, 5);
+            this.scaleX = 1;
+            this.scaleY = 1;
+            //console.log(this.width, this.height, this.scaleX, this.scaleY);
+            this.makeDirty();
+        });
+    },
+
     toObject: function () {
         return fabric.util.object.extend(this.callSuper("toObject"), {
             label: this.get("label"),
@@ -56,6 +76,7 @@ var SymbolButton = fabric.util.createClass(fabric.Rect, {
             linethrough: this.get("linethrough"),
             textAlign: this.get("textAlign"),
             textColor: this.get("textColor"),
+            imageSource: this.image.src,
         });
     },
 
@@ -100,8 +121,33 @@ var SymbolButton = fabric.util.createClass(fabric.Rect, {
         }
     },
 
+    drawImageScaled: function (ctx, x, y, width, height) {
+        var hRatio = width / this.image.width;
+        var vRatio = height / this.image.height;
+        var ratio = Math.min(hRatio, vRatio);
+        var centerShift_x = (width - this.image.width * ratio) / 2 + x;
+        var centerShift_y = (height - this.image.height * ratio) / 2 + y;
+        //ctx.clearRect(x, y, width, height);
+        ctx.drawImage(
+            this.image,
+            0,
+            0,
+            this.image.width,
+            this.image.height,
+            centerShift_x,
+            centerShift_y,
+            this.image.width * ratio,
+            this.image.height * ratio
+        );
+    },
+
     _render: function (ctx) {
         this.callSuper("_render", ctx);
+
+        console.log(this.scaleX, this.scaleY);
+
+        const imageMargin = 5; // margin around the image
+        const textYOffset = 20; // Y offset of text below the top of the button
 
         ctx.font = this.font;
         ctx.fillStyle = this.textColor;
@@ -109,9 +155,20 @@ var SymbolButton = fabric.util.createClass(fabric.Rect, {
         // in this coordinate system, 0.0 is in the center of the rectangle - we want to center the text
         var metrics = ctx.measureText(this.label);
         var x = -this.width / 2 + this.justifiedTextX(ctx, metrics);
-        var y = -this.height / 2 + 20;
+        var y = -this.height / 2 + textYOffset;
         ctx.fillText(this.label, x, y);
         this.decorateText(ctx, metrics, x, y);
+
+        x = -this.width / 2 + imageMargin; // x is left of the button plus the margin
+        y += metrics.fontBoundingBoxDescent + imageMargin; // y is bottom of the text plus the margin
+        var width = this.width - 2 * imageMargin; // width is button width minus margin on either side
+
+        // height is button height minus the Y offset of the text + text descent + image margin
+        var height =
+            this.height -
+            (textYOffset + metrics.fontBoundingBoxDescent + 2 * imageMargin);
+
+        this.drawImageScaled(ctx, x, y, width, height);
     },
 
     setLabel(cnv, label) {
