a924c2030afd3b839802edd005cc6a58bf28c3b2
Bob Cunningham <bobchm@gmail.com>
2022-11-14 13:42:10 -0500

    moved canvas-server to adaptable.io

--- src/utils/contentURL.js
+++ src/utils/contentURL.js
@@ -1,4 +1,5 @@
 export default function contentURL(suffix) {
     //return "http://localhost:5000/" + suffix;
-    return "https://canvas-server.onrender.com/" + suffix;
+    //return "https://canvas-server.onrender.com/" + suffix;
+    return "https://canvas-server.adaptable.app/" + suffix;
 }
