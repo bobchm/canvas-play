6199bfdeb171363e61a39dbe448db29880e074b6
Bob Cunningham <bobchm@gmail.com>
2023-01-09 20:50:29 -0500

    made hot spots have empty fill

--- src/utils/canvas.js
+++ src/utils/canvas.js
@@ -424,6 +424,7 @@ const addSymbolButton = (cnv, label, shape, spec, scrObj, inputCallback) => {
 
 const addHotSpot = (cnv, spec, scrObj, inputCallback) => {
     const newPath = new fabric.Path(spec.path);
+    newPath.set({ fill: "rgba(0,0,0,0)", stroke: "black", strokeWidth: 3 });
     finishObjectAdd(cnv, newPath, scrObj, inputCallback);
     return newPath;
 };
