d465f6e2c99ba6e895e7423462c1d43b464abca0
Bob Cunningham <bobchm@gmail.com>
2023-02-01 08:29:06 -0500

    formatting of print dialog, added paper formats

--- src/components/print-dialog/print-dialog.component.jsx
+++ src/components/print-dialog/print-dialog.component.jsx
@@ -21,9 +21,14 @@ const orientationOptions = [
 ];
 
 const formatOptions = [
-    { name: "Letter", value: "letter" },
-    { name: "Legal", value: "legal" },
+    { name: "A2", value: "A2" },
+    { name: "A3", value: "A3" },
     { name: "A4", value: "A4" },
+    { name: "A5", value: "A5" },
+    { name: "Ledger", value: "ledger" },
+    { name: "Legal", value: "legal" },
+    { name: "Letter", value: "letter" },
+    { name: "Tabloid", value: "tabloid" },
 ];
 
 export default function PrintDialog({
@@ -56,6 +61,13 @@ export default function PrintDialog({
         setAllPages(newValue === "all");
     };
 
+    function fixFileName(fname) {
+        if (!fname.toLowerCase().endsWith(".pdf")) {
+            return fname + ".pdf";
+        }
+        return fname;
+    }
+
     return (
         <div>
             <Dialog open={open} onClose={() => cancelCallback()}>
@@ -63,9 +75,9 @@ export default function PrintDialog({
                 <DialogContent>
                     <Stack
                         direction="row"
-                        alignItems="flex-start"
+                        alignItems="center"
                         justifyContent="center"
-                        spacing={2}
+                        //spacing={2}
                         sx={{ width: "100%" }}
                     >
                         <TextField
@@ -75,81 +87,91 @@ export default function PrintDialog({
                             value={fileName}
                             label="File Name"
                             type="text"
-                            sx={{ width: "70%" }}
+                            sx={{ width: "70%", mb: "20px" }}
                             variant="standard"
                             onChange={(event) =>
                                 setFileName(event.target.value)
                             }
                         />
-                        <Typography>pdf</Typography>
+                        <Typography>.pdf</Typography>
                     </Stack>
-                    <Select
-                        id="orientation"
-                        value={orientation}
-                        label="Orientation"
-                        sx={{
-                            width: "400px",
-                            fontSize: 13,
-                            height: 40,
-                            mt: 1,
-                            mb: 1,
-                        }}
-                        onChange={(e, child) =>
-                            handleOrientationChange(e.target.value)
-                        }
+                    <Stack
+                        direction="column"
+                        alignItems="flex-start"
+                        justifyContent="center"
+                        spacing={2}
                     >
-                        {orientationOptions.map((opt, idx) => (
-                            <MenuItem key={idx} value={opt.value}>
-                                {opt.name}
-                            </MenuItem>
-                        ))}
-                    </Select>
-                    <Select
-                        id="format"
-                        value={format}
-                        label="Format"
-                        sx={{
-                            width: "400px",
-                            fontSize: 13,
-                            height: 40,
-                            mt: 1,
-                            mb: 1,
-                        }}
-                        onChange={(e, child) =>
-                            handleFormatChange(e.target.value)
-                        }
-                    >
-                        {formatOptions.map((opt, idx) => (
-                            <MenuItem key={idx} value={opt.value}>
-                                {opt.name}
-                            </MenuItem>
-                        ))}
-                    </Select>
-                    <RadioGroup defaultValue="all" onChange={handlePagesChange}>
-                        <FormControlLabel
-                            value="all"
-                            control={<Radio />}
-                            label="All Pages"
+                        <Select
+                            id="orientation"
+                            value={orientation}
+                            label="Orientation"
+                            sx={{
+                                width: "400px",
+                                fontSize: 13,
+                                height: 40,
+                                mt: 1,
+                                mb: 1,
+                            }}
+                            onChange={(e, child) =>
+                                handleOrientationChange(e.target.value)
+                            }
+                        >
+                            {orientationOptions.map((opt, idx) => (
+                                <MenuItem key={idx} value={opt.value}>
+                                    {opt.name}
+                                </MenuItem>
+                            ))}
+                        </Select>
+                        <Select
+                            id="format"
+                            value={format}
+                            label="Format"
+                            sx={{
+                                width: "400px",
+                                fontSize: 13,
+                                height: 40,
+                                mt: 1,
+                                mb: 1,
+                            }}
+                            onChange={(e, child) =>
+                                handleFormatChange(e.target.value)
+                            }
+                        >
+                            {formatOptions.map((opt, idx) => (
+                                <MenuItem key={idx} value={opt.value}>
+                                    {opt.name}
+                                </MenuItem>
+                            ))}
+                        </Select>
+                        <RadioGroup
+                            defaultValue="all"
+                            onChange={handlePagesChange}
+                        >
+                            <FormControlLabel
+                                value="all"
+                                control={<Radio />}
+                                label="All Pages"
+                            />
+                            <FormControlLabel
+                                value="selected"
+                                control={<Radio />}
+                                label="Select Pages"
+                            />
+                        </RadioGroup>
+                        <TaggedAutocomplete
+                            tagOptions={pageOptions}
+                            tagCallback={handlePageNamesChange}
+                            tagsName="Pages"
+                            disabled={allPages}
                         />
-                        <FormControlLabel
-                            value="selected"
-                            control={<Radio />}
-                            label="Select Pages"
-                        />
-                    </RadioGroup>
-                    <TaggedAutocomplete
-                        tagOptions={pageOptions}
-                        tagCallback={handlePageNamesChange}
-                        tagsName="Pages"
-                        disabled={allPages}
-                    />
+                    </Stack>
                 </DialogContent>
                 <DialogActions>
                     <Button onClick={() => cancelCallback()}>Cancel</Button>
                     <Button
                         onClick={() =>
                             confirmCallback({
-                                filename: fileName + ".pdf",
+                                filename: fixFileName(fileName),
                                 orientation: orientation,
                                 format: format,
                                 pageList: allPages ? null : printPages,
